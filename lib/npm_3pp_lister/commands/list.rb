0ac4b50f8fd54ac0048c6326aefc332613fe6dc8
Eric Khoury <eric.x.khoury@ericsson.com>
2018-12-19 14:45:01 +0100

    Update info, package gem

--- lib/npm_3pp_lister/commands/list.rb
+++ lib/npm_3pp_lister/commands/list.rb
@@ -12,16 +12,21 @@ module Npm3ppLister
       end
 
       def execute(input: $stdin, output: $stdout)
-        file = File.read('./package.json')
-        json = JSON.parse(file)
+        if File.file?('./package.json')
+          file = File.read('./package.json')
+          json = JSON.parse(file)
 
-        json["dependencies"].each do |key, val|
-          File.open("./3pp_list.txt","a") do |f|
-            output.puts "#{key.colorize(:yellow)}: #{val}" + " " + "https://registry.npmjs.org/#{key}/-/#{key}-#{val.gsub("^", "").gsub("~", "")}.tgz".colorize(:green)
-            f.puts "#{key}:#{val} - https://registry.npmjs.org/#{key}/-/#{key}-#{val.gsub("^", "").gsub("~", "")}.tgz"
+          json["dependencies"].each do |key, val|
+            File.open("./3pp_list.txt","a") do |f|
+              output.puts "#{key.colorize(:yellow)}: #{val}" + " " + "https://registry.npmjs.org/#{key}/-/#{key}-#{val.gsub("^", "").gsub("~", "")}.tgz".colorize(:green)
+              f.puts "#{key}:#{val} - https://registry.npmjs.org/#{key}/-/#{key}-#{val.gsub("^", "").gsub("~", "")}.tgz"
+            end
           end
+          output.puts "\n\nResult written to 3pp_list.txt"
+
+        else
+          output.puts "- No package.json found here -"
         end
-        output.puts "\n\nResult written to 3pp_list.txt"
       end
     end
   end
--- spec/npm_3pp_lister_spec.rb
+++ spec/npm_3pp_lister_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe Npm3ppLister do
-  it "has a version number" do
-    expect(Npm3ppLister::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
